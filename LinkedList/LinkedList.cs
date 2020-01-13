5348b2656fee79dab4f7703c81a3e483e6784bfa
AntonNorberg <antonnorberg98@gmail.com>
2020-01-13 12:02:13 +0100

    Linked list package

---
+++ LinkedList/LinkedList.cs
@@ -0,0 +1,51 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Datastructures.LinkedList
+{
+    class LinkedListNode
+    {
+        public LinkedListNode next;
+        public Object data;
+    }
+    class LinkedList
+    {
+        private LinkedListNode head;
+
+        public void PrintAllNodes()
+        {
+            LinkedListNode current = head;
+            while(current != null)
+            {
+                Console.WriteLine(current.data);
+                current = current.next;
+            }
+        }
+
+        public void AddLast(Object data)
+        {
+            if(head == null)
+            {
+                head = new LinkedListNode();
+
+                head.data = data;
+                head.next = null;
+            } else
+            {
+                LinkedListNode toAdd = new LinkedListNode();
+                toAdd.data = data;
+
+                LinkedListNode current = head;
+                while (current.next != null)
+                {
+                    current = current.next;
+                }
+
+                current.next = toAdd;
+            }
+        }
+
+
+    }
+}
